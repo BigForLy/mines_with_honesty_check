ae8e980e8de1d1cae7bc242ae3af95573c8bcf7d
BigForLy <oringan@mail.ru>
2022-05-25 16:47:48 +0500

    feat: swagger created

--- mines/authentication/urls.py
+++ mines/authentication/urls.py
@@ -7,5 +7,5 @@ from .views import (
 urlpatterns = [
     path('signup/', RegistrationAPIView.as_view(), name='signup'),
     path('signin/', LoginAPIView.as_view(), name='signin'),
-    path('user/', UserRetrieveUpdateAPIView.as_view()),
+    path('', UserRetrieveUpdateAPIView.as_view(), name='user'),
 ]
