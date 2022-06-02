545b1e70f1a36a27df67f4b2e72195224da3fe41
BigForLy <oringan@mail.ru>
2022-06-02 18:09:18 +0500

    feat: create tests on move bomb game

--- mines/bomb_game/tests/test_view.py
+++ mines/bomb_game/tests/test_view.py
@@ -1,8 +1,8 @@
-from ..tests.tets_setup import TestSetUp, TestSetUpAlreadyCreatedModel
+from ..tests.tets_setup import TestStartGameSetUp, TestSetUpAlreadyCreatedModel, TestMoveGameSetUp
 from rest_framework import status
 
 
-class TestBombStartGameViews(TestSetUp):
+class TestBombStartGameViews(TestStartGameSetUp):
 
     def test_start_game_without_data(self):
         response = self.client.post(
@@ -13,55 +13,55 @@ class TestBombStartGameViews(TestSetUp):
     def test_start_game_correctly(self):
         response = self.client.post(
             self.start_bomb_game_url,
-            self.bomb_game_data
+            self.bomb_game_start_data
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_start_game_incorrect_min_bomb(self):
-        self.bomb_game_data["bomb"] = 0
+        self.bomb_game_start_data["bomb"] = 0
         response = self.client.post(
             self.start_bomb_game_url,
-            self.bomb_game_data
+            self.bomb_game_start_data
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_start_game_incorrect_max_bomb(self):
-        self.bomb_game_data["bomb"] = 25
+        self.bomb_game_start_data["bomb"] = 25
         response = self.client.post(
             self.start_bomb_game_url,
-            self.bomb_game_data
+            self.bomb_game_start_data
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_start_game_without_bomb(self):
-        self.bomb_game_data.pop("bomb")
+        self.bomb_game_start_data.pop("bomb")
         response = self.client.post(
             self.start_bomb_game_url,
-            self.bomb_game_data
+            self.bomb_game_start_data
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_start_game_with_excess_money(self):
-        self.bomb_game_data["start_sum"] = 99999999999999999999999
+        self.bomb_game_start_data["start_sum"] = 99999999999999999999999
         response = self.client.post(
             self.start_bomb_game_url,
-            self.bomb_game_data
+            self.bomb_game_start_data
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_start_game_with_lack_money(self):
-        self.bomb_game_data["start_sum"] = 0
+        self.bomb_game_start_data["start_sum"] = 0
         response = self.client.post(
             self.start_bomb_game_url,
-            self.bomb_game_data
+            self.bomb_game_start_data
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_start_game_witout_money(self):
-        self.bomb_game_data.pop("start_sum")
+        self.bomb_game_start_data.pop("start_sum")
         response = self.client.post(
             self.start_bomb_game_url,
-            self.bomb_game_data
+            self.bomb_game_start_data
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
@@ -71,6 +71,70 @@ class TestBombStartGameAlreadyCreatedViews(TestSetUpAlreadyCreatedModel):
     def test_start_game_already_exists(self):
         response = self.client.post(
             self.start_bomb_game_url,
-            self.bomb_game_data
+            self.bomb_game_start_data
         )
         self.assertEqual(response.status_code, status.HTTP_409_CONFLICT)
+
+
+class TestBombMoveGameNoCreatedViews(TestStartGameSetUp):
+
+    def test_move_game_without_data(self):
+        response = self.client.post(
+            self.move_bomb_game_url
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class TestBombMoveGameViews(TestMoveGameSetUp):
+
+    def test_move_game_without_data(self):
+        response = self.client.post(
+            self.move_bomb_game_url
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_move_game_correctly(self):
+        response = self.client.post(
+            self.move_bomb_game_url,
+            self.bomb_game_move_data
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_move_game_incorrect_min_move(self):
+        self.bomb_game_move_data["move"] = -1
+        response = self.client.post(
+            self.move_bomb_game_url,
+            self.bomb_game_move_data
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_move_game_incorrect_max_move(self):
+        self.bomb_game_move_data["move"] = 26
+        response = self.client.post(
+            self.move_bomb_game_url,
+            self.bomb_game_move_data
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_move_game_duplicate(self):
+        response = self.client.post(
+            self.move_bomb_game_url,
+            self.bomb_game_move_data
+        )
+        # проверяем что ход успешный
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response = self.client.post(
+            self.move_bomb_game_url,
+            self.bomb_game_move_data
+        )
+        self.assertEqual(response.status_code, status.HTTP_409_CONFLICT)
+
+    def test_move_game_lose(self):
+        self.bomb_game_move_data["move"] = 1
+        response = self.client.post(
+            self.move_bomb_game_url,
+            self.bomb_game_move_data
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data.get("bomb_in")),
+                         self.bomb_game_start_data.get("bomb"))
--- mines/bomb_game/tests/tets_setup.py
+++ mines/bomb_game/tests/tets_setup.py
@@ -1,14 +1,16 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from django_redis import get_redis_connection
+from django.conf import settings
 
 
-class TestSetUp(APITestCase):
+class TestStartGameSetUp(APITestCase):
 
     def setUp(self) -> None:
         self.start_bomb_game_url = reverse('start_bomb_game')
+        self.move_bomb_game_url = reverse('move_bomb_game')
 
-        self.bomb_game_data = {
+        self.bomb_game_start_data = {
             "bomb": 3,
             "start_sum": 10
         }
@@ -36,11 +38,25 @@ class TestSetUp(APITestCase):
         return super().tearDown()
 
 
-class TestSetUpAlreadyCreatedModel(TestSetUp):
+class TestSetUpAlreadyCreatedModel(TestStartGameSetUp):
 
     def setUp(self) -> None:
         super().setUp()
         self. created_game = self.client.post(
             self.start_bomb_game_url,
-            self.bomb_game_data
+            self.bomb_game_start_data
         )
+
+
+class TestMoveGameSetUp(TestSetUpAlreadyCreatedModel):
+
+    def setUp(self) -> None:
+        self.bomb_game_move_data = {
+            "move": 3
+        }
+        range_element = settings.BOMB_GAME_COUNT_ELEMENT
+        # ограничиваем область, чтобы точно быть уверенными где бомбы
+        settings.BOMB_GAME_COUNT_ELEMENT = range(0, 3)
+        super().setUp()
+        # восстанавливаем изначальную последовательность
+        settings.BOMB_GAME_COUNT_ELEMENT = range_element
